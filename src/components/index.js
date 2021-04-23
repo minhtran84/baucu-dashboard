ce92bc6e9dbdd1c944b602afff63cea1018f9fac
Minh Tran <minhtran.se@gmail.com>
2021-04-23 15:39:38 +0700

    define new api url for new app

--- src/components/index.js
+++ src/components/index.js
@@ -12,14 +12,6 @@ export { default as Doughnuttiendohuyen } from './Doughnuttiendohuyen/Doughnutti
 
 export { default as Doughnuttiendoxa } from './Doughnuttiendoxa/Doughnuttiendoxa';
 
-export { default as Pietiendotinh } from './Pietiendotinh/Pietiendotinh';
-
-export { default as Pietiendoqh } from './Pietiendoqh/Pietiendoqh';
-
-export { default as Pietiendohuyen } from './Pietiendohuyen/Pietiendohuyen';
-
-export { default as Pietiendoxa } from './Pietiendoxa/Pietiendoxa';
-
 export { default as Barsolieutinh} from './Barsolieutinh/Barsolieutinh';
 
 export { default as Barsolieuqh} from './Barsolieuqh/Barsolieuqh';
